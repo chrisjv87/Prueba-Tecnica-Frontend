e345c3162ac9380e52c910e705b9f3bb523e5c78
ChrisValDev <jimenez.valverde0805@gmail.com>
2024-12-27 18:40:46 -0600

    Initial structure

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './styles/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a href="/">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
