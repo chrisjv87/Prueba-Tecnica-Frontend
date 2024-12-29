84c376fd6eec3fad7ec8207b57fd638e401ad2fc
ChrisValDev <jimenez.valverde0805@gmail.com>
2024-12-29 00:13:05 -0600

    Prepare project for deployment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.scss";
-// import CargoOrders from "./components/CargoOrders/CargoOrders";
+import CargoOrders from "./components/CargoOrders/CargoOrders";
 import CargoDetails from "./components/CargoDetails/CargoDetails";
 
 function App() {
   return (
     <div className="App">
-      {/* <CargoOrders /> */}
+      <CargoOrders />
       <CargoDetails />
     </div>
   );
