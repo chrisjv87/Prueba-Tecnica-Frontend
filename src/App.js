c56a4b080081bb075d67649ea6d11b0c77bf1866
ChrisValDev <jimenez.valverde0805@gmail.com>
2024-12-28 20:36:39 -0600

    CargoDetails layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.scss";
-import CargoOrders from "./components/CargoOrders/CargoOrders";
+// import CargoOrders from "./components/CargoOrders/CargoOrders";
+import CargoDetails from "./components/CargoDetails/CargoDetails";
 
 function App() {
   return (
     <div className="App">
-      <CargoOrders />
+      {/* <CargoOrders /> */}
+      <CargoDetails />
     </div>
   );
 }
---
+++ src/components/CargoDetails/CargoDetails.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Header/Header';
+import './CargoDetails.scss';
+
+const CargoDetails = () => {
+  return (
+    <div className="cargo-details">
+      <Header text="Cargo Details" />
+
+    </div>
+  );
+}
+
+export default CargoDetails;
